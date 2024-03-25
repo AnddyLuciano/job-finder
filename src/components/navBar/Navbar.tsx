39feb07a01517ba617b5f7d87a830b86a0c0100f
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2024-03-25 07:15:33 +0100

    feature: navbar

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useMenuList } from "../../hooks/useMenuList";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const menulists = useMenuList();
+    return (
+        <div className="flex items-center justify-between py-8">
+            <div className="logo-box">
+                <p className="text-xl">
+                    <strong className="text-primary">Job</strong> Finder
+                </p>
+            </div>
+            <div className="menu-box flex gap-10">
+                {menulists.map((list) => (
+                    <li key={list.key} className="menuList hover:text-primary">
+                        {list.item}
+                    </li>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
