452723e53162a5c1f98d3e34b46b9dc6031527db
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2024-03-26 18:58:44 +0100

    update: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import Footer from "./components/footerDiv/Footer";
+import Job from "./components/jobDiv/Job";
+import Navbar from "./components/navBar/Navbar";
+import Search from "./components/searchDiv/Search";
+import Value from "./components/valueDiv/Value";
 
 function App() {
-    return <div>test</div>;
+    return (
+        <div className="w-[85%] m-auto text-sm">
+            <Navbar />
+            <Search />
+            <Job />
+            <Value />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
